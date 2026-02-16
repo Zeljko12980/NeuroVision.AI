092028ce70219fd075cc79a72be1e33839bbb33a
ikano <ikanoviczeljko095@gmail.com>
2026-02-16 23:25:24 +0100

    Add MailService API with .NET 9.0 support
    Created and configured MailService API, Application, Domain, and Infrastructure projects targeting .NET 9.0. Added HTTP configuration, logging settings, and updated Program.cs for service setup. Included project references in AppHost and updated solution structure to incorporate new projects.

--- src/neurovision.ai/neurovision.ai.AppHost/AppHost.cs
+++ src/neurovision.ai/neurovision.ai.AppHost/AppHost.cs
@@ -16,4 +16,7 @@ builder.AddProject<Projects.IdentityService_API>("identityservice-api")
 builder.AddProject<Projects.DoctorService_API>("doctorservice-api");
 
 
+builder.AddProject<Projects.MailService_API>("mailservice-api");
+
+
 builder.Build().Run();
